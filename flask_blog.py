d162859be2c8b040a6a60b32a6e027ecfc56128f
ShubhamNair <shubhamk.nair@gmail.com>
2024-04-03 02:08:31 -0700

    Login and registration added

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template, url_for
 from markupsafe import escape
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '24fe825fc80a9203826c5a7f54023867'
 
 posts=[
     {
@@ -26,6 +29,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html",title="About")
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form= form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form= form)
 
 if __name__ == "__main__":
     app.run(debug=True)
